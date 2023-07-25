5006c9b7e24e2f75dc4c5c8cf501aa290c31ee45
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-07-25 21:56:33 +0530

    code changes

--- binary_search/Insert_Position.java
+++ binary_search/Insert_Position.java
@@ -5,18 +5,18 @@ public class Insert_Position {
         System.out.println(searchInsert(nums,target));
     }
     public static int searchInsert(int[] nums, int target) {
-        int s = 0;
-        int e = nums.length - 1;
-            while (s <= e) {
-                int m = s + (e - s) / 2;
-                if (nums[m] > target) {
-                    e = m - 1;
-                } else if (nums[m] < target) {
-                    s = m+1;
+        int start = 0;
+        int end = nums.length - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] > target) {
+                    end = mid - 1;
+                } else if (nums[mid] < target) {
+                    start = mid+1;
                 } else {
-                    return m;
+                    return mid;
                 }
             }
-    return s;
+    return start;
     }
 }
