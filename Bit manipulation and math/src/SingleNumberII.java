7a74ad6ee24f18dc38999867ca628f0c70d47d49
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-07-26 14:10:05 +0530

    commiting files

--- Bit manipulation and math/src/SingleNumberII.java
+++ Bit manipulation and math/src/SingleNumberII.java
@@ -1,24 +1,40 @@
-import java.util.Arrays;
 
-public class SingleNumberIII {
+
+public class SingleNumberII {
     public static void main(String[] args) {
-        int [] nums = {1,2,1,3,2,5};
-//        Output: [3,5]
-//        Explanation:  [5, 3] is also a valid answer.
-        System.out.println(singleNumber(nums));
+        int [] nums = {2,2,3,2};
+        System.out.println(singleNumber1(nums));
+//        nums = [2,2,3,2]
+//Output: 3
     }
-    public static int[] singleNumber(int[] nums) {
-        int[] arr = new int[2];
-        int ans = 0;
-        for(int i = 0 ; i < nums.length;i++){
-          ans ^=nums[i];
+    public static int singleNumber(int[] nums) {
+        int ones = 0 , twos = 0 ;
+        //we use property of ^ -> a^a=0; a^0 = a;
+        for( int i = 0; i< nums.length;i++){
+            //ones is only updated to value of num if there is nothing in twos-> ~twos returnes 1
+            //if twos contains nums then ~ twos will return 0 and hence ones wont be updated because it will be a count for three and not ones
+            //order of numbers dosent matter as internally at bit levels operations are taking place
+            ones = ( ones ^ nums[i]) & ~ twos;
+            //same way for twos , only update twos if one is not present
+            twos = ( twos ^ nums[i]) & ~ ones;
         }
-        System.out.println(Arrays.toString(nums));
-        int o = 0;
-        for( int i : nums){
-            if(i !=0 ){
-                arr[o++] = i;
+        return ones;
+        //O(n) and O(1) t and s c
+    }
+    //in this approach we are counting the occurence of bits in each position and in the we are dividing
+    //the sum of all bit occurences by 3 , if remainder is 0 it means that all the bits have occured 3 times
+    //if one number has occures one time then the remainder wont be 0 and equal to that number
+    public static int singleNumber1(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for( final int num : nums){
+                //note that we are counting the 1st bit of all the number and then second and soo on
+                sum += num >> i & 1 ;//counts the number of 1's at the bit position
+                sum%=3;
+                ans |= sum << i;//updating the ans
             }
-        }return arr;
+        }
+        return ans;
     }
 }
