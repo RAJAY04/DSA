56974039808036de26eb59cb07eaf481b70f2c13
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-07-29 13:04:37 +0530

    daily code

--- strings/LongestSubstring.java
+++ strings/LongestSubstring.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class LongestSubstring {
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+//        Output: 3
+//        Explanation: The answer is "abc", with the length of 3.
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    //using hashmap will greatly reduce the time complexity , this has very bad time complexity
+    //we use 2 pointer approach to track the substring
+    public static int lengthOfLongestSubstring(String s) {
+        if(s.length()==0){
+            return 0;
+        } else if (s.length()==1) {
+            return 1;
+        }
+        int length = 1;
+        int i = 0;
+        int j = 1;
+        while( j < s.length()){
+            int temp = j-1;
+            boolean check = true;
+            while(temp >= i) {
+                if (s.charAt(temp) == s.charAt(j)) {
+                    check = false;
+                }
+                temp--;
+            }if(check){
+                j++;
+                length =Math.max(length,j-i);
+            }else{
+                i++;
+                length =Math.max(length,j-i);
+            }
+        }return length;
+    }
 }
