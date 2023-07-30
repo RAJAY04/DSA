46b715a5e71b0f183ce8204c176de3df2f20d318
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-07-30 16:59:37 +0530

    leetcode weekly challenged missed 1st question

---
+++ LeetcodeContest/src/EmployeMetTarget.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class EmployeMetTarget {
+    public static void main(String[] args) {
+        int[] hours = {1,4,6,99,66,77,56};
+        int target =57;
+        System.out.println(numberOfEmployeesWhoMetTarget(hours,target));
+//        Output: 3
+//        Explanation: The company wants each employee to work for at least 2 hours.
+//                - Employee 0 worked for 0 hours and didn't meet the target.
+//                - Employee 1 worked for 1 hours and didn't meet the target.
+//                - Employee 2 worked for 2 hours and met the target.
+//                - Employee 3 worked for 3 hours and met the target.
+//                - Employee 4 worked for 4 hours and met the target.
+//                There are 3 employees who met the target.
+    }
+    public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        Arrays.sort(hours);//sort the array to use binary search
+        int s = 0;
+        int e = hours.length-1;
+        while( s <= e ){
+            int mid = s + ( e - s )/2;
+            //if target hours greater than shift start to right
+            if( hours[mid] < target){
+                s = mid + 1;
+                //if target hours less or equal to mid it means that there may be more probable answer leftside
+                //hence shift the end to left side
+            }else{
+                e=mid-1;
+            }
+            //in the end start will be greater than end hence loop breaks
+        }return hours.length-s;
+    }
+
+}
