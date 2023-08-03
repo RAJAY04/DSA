a47d9826b60ce76427bfc9a45220f5af99c56295
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-08-03 23:23:20 +0530

    daily codes

---
+++ recursion/TrianglePattern_L.java
@@ -0,0 +1,30 @@
+public class TrianglePattern_L {
+    public static void main(String[] args) {
+        triangle2(4,0);
+    }
+    static void triangle ( int r , int c){
+        if( r == 0 ){
+            return;
+        }
+        if(c < r){
+            System.out.print("*");
+            triangle(r, c+1);
+        }  else{
+            System.out.println();
+            triangle(r-1,0);
+        }
+    }
+    //to print a proper triangle just print the stars while comming out of the function in the end
+    static void triangle2 ( int r , int c){
+        if( r == 0 ){
+            return;
+        }
+        if(c < r){
+            triangle2(r, c+1);
+            System.out.print("*");
+        }  else{
+            triangle2(r-1,0);
+            System.out.println();
+        }
+    }
+}
