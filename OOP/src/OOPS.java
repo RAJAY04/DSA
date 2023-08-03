dc78f00277e9695dbfb40de745328cab9d4ad2b9
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-08-03 22:15:10 +0530

    oops code

--- OOP/src/OOPS.java
+++ OOP/src/OOPS.java
@@ -11,9 +11,13 @@ public class OOPS {
         System.out.println(ajay.rollNo);
         System.out.println(ajay.name);
         System.out.println(ajay.marks);
-        ajay.greeting();
+
+
+
+        ajay.greeting();//check name before using changeName method
         ajay.changeName("vijay");
-        ajay.greeting();
+        ajay.greeting();//check name after using changeName method
+
 
 
         Student kunal = new Student( 40 , 34f,"kunal");
@@ -22,12 +26,26 @@ public class OOPS {
         System.out.println(kunal.marks);
 
 
+
         //passing a object inside constructor
         Student random = new Student(kunal);
         System.out.println(random.name);
+
+
+
+        //checking a constructor calling another constructor
+        Student random2 = new Student();
+        System.out.println(random2.name);
+
+
+
+        //we see below how two names point to the same object
+        Student one = new Student();
+        Student two = one;
+        one.name="something something";
+        System.out.println(two.name);
+        //we can see that changes made via one will effect two and vice versa
         }
-
-
 }
     class Student{
         int rollNo = 38;//default value;
@@ -51,22 +69,40 @@ public class OOPS {
             this.marks = other.marks;
         }
 
-        Student(){
-            this.name="ajay";
-            this.marks=10;
-            this.rollNo=41;
+        //note: if we dont specify any constructor at all then java has a default constructor that sets
+        //the default value ,eg null
+
+//        Student(){//dosent take arguments and sets all values of objects created as defined below
+//            this.name="ajay";
+//            this.marks=10;
+//            this.rollNo=41;
             //the this keyword is used for any object we create , eg if we create a object named ajay
             //then the this is replaced by ajay and all the initialisation takes place
-        }
+
         //we see that both the constructors have the same name hence depending on if you pass the
-        //arguments or not it will call the constructor respectiveely this is called constructor overloading
-        Student(int rollNo , float marks , String name){
-//            rollNo = rollNo;
+        //arguments or not it will call the constructor respectively this is called constructor overloading
+
+
+
+
+        Student() {
+            //internally its : Student( 0,100f,"Default");
+            this ( 0,100f,"Default");
+        }//this means the constructor which is calling here , in this case
+        //this is getting replaced with Student() with specified arguments
+
+
+
+
+        //            rollNo = rollNo;
 //            marks = marks;this keyword basically means what object you are reffering to
 //            name = name; this gives error when name is same hence we can use this keyword here
+        Student(int rollNo , float marks , String name){
             this.rollNo = rollNo;
             this.marks = marks;
             this.name = name;
+            //this is the correct way of initialising objects , we can create as much objects we want
+            //by passing the arguments as specified int the constructor body
 //we can see that when we have changed the argument name slightly now it works
 //hence the conclusion is better use this keyword , as it work even if the name is same;
         }
---
+++ OOP/src/WrapperClass_imp.java
@@ -0,0 +1,66 @@
+public class WrapperClass_imp {
+    public static void main(String[] args) {
+        //wkt in java there is no pass by reference in primitives
+        int x = 10;
+        //we see that when we do a. we can see any methods available because its not a object
+        Integer A = 20;//starting from java 11 we cant do Integer A = new Integer(20);
+        // we can do
+//        Integer A = Integer.valueOf(20); but no need
+        //but here when we do A. we can see that there are lots of methods available as now
+        //it has become a object
+
+        int a = 10;
+        int b = 20;
+        swap(a,b);
+        System.out.println(a+" "+b);//we can see that its still not swapped
+        //it is just passing the value and its getting swapped inside the function , the
+        //original one dosent get effected as its a primitive and not a object
+        //soo what if we make a object using Integer wrapper class?
+        //even then it wont swap because Integer is final
+
+
+        //use of final
+//        final int INCREASE = 1;//by convention make it all caps if its final
+//        INCREASE = 2; this gives error
+
+
+       final A kunal = new A("new object");
+        System.out.println(kunal.name);
+       kunal.name = " other name";
+//when a non primitive is final you cannot resign it , but you can change the value
+//       kunal = new A("new object");
+        System.out.println(kunal.name);
+
+
+
+        //finalizer
+        A obj;
+        //soo many objets are created and we know only one object and point 1 reference variable
+        for (int i = 0; i < 1000000; i++) {//note that for small number of objects it dosent destroy
+            obj = new A("i am a object");
+        }
+
+    }
+    static void swap( int a , int b){
+        int temp = a ;
+        a = b;
+        b = temp;
+    }
+
+}
+class A{
+    final int num = 10;
+    String name;
+
+    public A(String name){// this is a constructor
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        //we cant control the destruction of the objects , but we can tell java to do something when
+        //it automatically destroys the object
+        System.out.println("Object has been destroyed");
+    }
+
+}
