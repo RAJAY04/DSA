49c4cab5ff58fe7e2ba39eb0cc4848ad988117b7
RAJAY04 <122152545+RAJAY04@users.noreply.github.com>
2023-08-08 20:17:43 +0530

    daily codes

--- recursion/RBS.java
+++ recursion/RBS.java
@@ -1,7 +1,7 @@
 public class RBS {
     public static void main(String[] args) {
         int[] arr = { 5,6,7,8,9,1,2,3};
-        int target = 4;
+        int target = 3;
         System.out.println(search(arr,target,0,arr.length-1));
     }
     static int search( int[] arr , int target , int s , int e){
@@ -21,8 +21,9 @@ public class RBS {
         }
             if( target >= arr[m] && target <= arr[e]){
                 return search(arr,target,m+1,e);
-            }
+            }else{
                 return search(arr,target,s,m-1);
-        }
+           }
+    }
     }
 
